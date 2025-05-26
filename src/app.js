c438274c8be8a3734ee64ef2d358e6d865054c4e
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-05-26 22:50:57 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  console.log(req);
+  res.send({ firstname: "Ankit", lastname: "Pareek" });
+});
 app.use("/test", (req, res) => {
   res.send("Hello from the test page for github");
-});
-app.use("/", (req, res) => {
-  res.send("Hello from the dashboard!");
 });
 
 app.listen(3000, () => {});
