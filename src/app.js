caeff9d55dab0c56cfda0f22dea71d83a1ec08d4
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-05-26 15:13:07 +0530

    added nodemon

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use("/test", (req, res) => {
+  res.send("Hello from the test page");
 });
+app.use("/", (req, res) => {
+  res.send("Hello from the dashboard!");
+});
+
+app.listen(3000, () => {});
