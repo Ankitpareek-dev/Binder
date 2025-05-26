d63001ace298d1d0652d1a2bf237891308f48283
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-05-26 15:14:36 +0530

    changing for the sake of commit message

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/test", (req, res) => {
-  res.send("Hello from the test page");
+  res.send("Hello from the test page for github");
 });
 app.use("/", (req, res) => {
   res.send("Hello from the dashboard!");
