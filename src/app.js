ced1c90b207e5b6d78c484757796aaad52afda2d
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-05-28 01:43:27 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -2,7 +2,28 @@ const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/auth");
 const app = express();
 
+const User = require("./models/user");
 const connectDB = require("./config/database");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Ankit",
+    lastName: "Pareek",
+    emailId: "ankit@pareek.com",
+    password: "xyz",
+    age: 12,
+    gender: "male",
+  });
+
+  // error handling
+  try {
+    await user.save();
+    res.send("User created successfully");
+  } catch (err) {
+    console.error("Error saving user:", error);
+    return res.status(500).send("Error creating user");
+  }
+});
 
 connectDB()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://ankitpareekdev:aEEPV2AyF3B8zBK4@hellodb.tim6dob.mongodb.net"
+    "mongodb+srv://ankitpareekdev:aEEPV2AyF3B8zBK4@hellodb.tim6dob.mongodb.net/devTinder"
   );
 };
 
