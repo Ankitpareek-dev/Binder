c55f56931fe6b89ca6f40fc55dae17e0e7121c5d
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-05-28 23:21:41 +0530

    api for fetching users all and by email

--- src/app.js
+++ src/app.js
@@ -4,10 +4,31 @@ const app = express();
 
 const User = require("./models/user");
 const connectDB = require("./config/database");
+const { model } = require("mongoose");
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+// GET api to fetch all users
+app.get("/feed", async (req, res) => {
+  const allUser = await User.find({});
+  res.send(allUser);
+});
+
+// GET api to fetch a specific user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  // console.log(userEmail);
+
+  try {
+    res.send(await User.find({ emailId: userEmail }));
+  } catch (err) {
+    console.error("Error fetching user:", err);
+    return res.status(500).send("Error fetching user");
+  }
+});
+
+// POST api to create a new user
 app.post("/signup", async (req, res) => {
   // Extracting user data from request body
   const user = new User(req.body);
