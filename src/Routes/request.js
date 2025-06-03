43d0a61242490e1a6a4980cac691443c8d66702c
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-04 01:43:06 +0530

    added a request Sending api

--- src/Routes/request.js
+++ src/Routes/request.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequestModel = require("../models/connectionRequest");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  console.log("Sending a connection request");
+requestRouter.post(
+  "/sendConnectionRequest/:status/:userId",
+  userAuth,
+  async (req, res) => {
+    const user = req.user;
+    const toUserId = req.params.userId;
+    const fromUserId = user._id;
 
-  res.send(user.firstName + " send the connection request");
-});
+    try {
+      const existingRequest = await ConnectionRequestModel.findOne({
+        fromUserId,
+        toUserId,
+      });
+      if (existingRequest) {
+        return res.status(400).json({ message: "Request already sent" });
+      }
+      const newRequest = new ConnectionRequestModel({
+        fromUserId,
+        toUserId,
+        status: req.params.status,
+      }).save();
+      res.send({
+        message: "Connection request sent successfully",
+        request: newRequest,
+      });
+    } catch (error) {
+      console.error("Error sending connection request:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
 
 module.exports = requestRouter;
---
+++ src/models/connectionRequest.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: "{VALUE} is not a valid status",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const connectionRequestModel = mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+
+module.exports = connectionRequestModel;
