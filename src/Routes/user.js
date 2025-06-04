646a0d64e1d90ff8c593f3fbb07f424e91744c7f
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-05 02:31:36 +0530

    added a constant for safe user data

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -3,6 +3,8 @@ const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const connectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -11,13 +13,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
         toUserId: loggedInUser._id,
         status: "interested",
       })
-      .populate("fromUserId", [
-        "firstName",
-        "lastName",
-        "photoUrl",
-        "age",
-        "gender",
-      ]);
+      .populate("fromUserId", USER_SAFE_DATA);
 
     res.json({
       connectionRequests,
@@ -38,7 +34,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
           { toUserId: loggedInUser._id, status: "accepted" },
         ],
       })
-      .populate("fromUserId", ["firstName"]);
+      .populate("fromUserId", USER_SAFE_DATA);
     res.send(connections);
   } catch (error) {
     res.status(400).json({ message: "Internal server error" });
