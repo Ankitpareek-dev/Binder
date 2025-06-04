5c7f43288fb6b2fc6ea2288f2a5bcd298c615d17
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-05 02:12:17 +0530

    added an api to see all the accepted request of a particular user

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -3,17 +3,46 @@ const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const connectionRequest = require("../models/connectionRequest");
 
-userRouter.get("user/requests", userAuth, async (req, res) => {
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
-    const connectionRequests = await connectionRequest.find({
-      toUserId: loggedInUser._id,
-      status: "interested",
+    const connectionRequests = await connectionRequest
+      .find({
+        toUserId: loggedInUser._id,
+        status: "interested",
+      })
+      .populate("fromUserId", [
+        "firstName",
+        "lastName",
+        "photoUrl",
+        "age",
+        "gender",
+      ]);
+
+    res.json({
+      connectionRequests,
     });
   } catch (error) {
     res.status(400).json({ message: "Internal server error" });
   }
 });
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connections = await connectionRequest
+      .find({
+        $or: [
+          { fromUserId: loggedInUser._id, status: "accepted" },
+          { toUserId: loggedInUser._id, status: "accepted" },
+        ],
+      })
+      .populate("fromUserId", ["firstName"]);
+    res.send(connections);
+  } catch (error) {
+    res.status(400).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = userRouter;
-b;
