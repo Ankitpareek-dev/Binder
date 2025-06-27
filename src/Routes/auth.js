5a1235cb98e8180d1d182ca5acf7a866a12281be
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-27 12:48:55 +0530

    testing deployment again

--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -40,7 +40,12 @@ authRouter.post("/login", async (req, res) => {
         expiresIn: "1d",
       });
       // Setting the token in cookies
-      res.cookie("token", token);
+      res.cookie("token", token, {
+        httpOnly: true, // can't be accessed from JS
+        secure: isProd, // true if on HTTPS (Render), false for localhost
+        sameSite: "none", // allow cross-site cookie setting
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+      });
       res.send({
         message: "Login successful",
         user: {
